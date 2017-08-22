bc750b391dd8df3878e98c3d1937e6c15c3030f2
Matt Fujita <matthew.fujita@libertymutual.com>
2017-08-22 15:11:12 -0700

    initial rough draft of roman numerals

---
+++ src/main/java/com/liberymutual/goforcode/romanNumerals/services/HinduToRomanNumeralConverter.java
@@ -0,0 +1,27 @@
+package com.liberymutual.goforcode.romanNumerals.services;
+
+public class HinduToRomanNumeralConverter {
+	
+	public HinduToRomanNumeralConverter() {
+		String[] symbols = {"I", "V", "X", "L", "C", "D", "M"}; //length of 7
+		String[] getNumber = {"1", "2", "3", "4", "5", "6", "7", "8", "9"};
+		String result = "";
+
+//		I("I", 1),
+//		V("V", 5),
+//		X("X", 10),
+//		L("L", 50),
+//		C("C", 100),
+//		D("D", 500),    
+//		M("M", 1000);
+	}
+
+	public String convert(int numberToConvert) {
+		//1 = I
+		//4 = I + V
+		//for loop
+		}
+		return null;
+	}
+
+}
