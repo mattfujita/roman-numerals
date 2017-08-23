b5839d6cdcf8d612e3881343ed83b4cba73ac9bb
Matt Fujita <matthew.fujita@libertymutual.com>
2017-08-22 17:18:18 -0700

    roman-numeral assignment

--- src/main/java/com/liberymutual/goforcode/romanNumerals/services/HinduToRomanNumeralConverter.java
+++ src/main/java/com/liberymutual/goforcode/romanNumerals/services/HinduToRomanNumeralConverter.java
@@ -2,26 +2,69 @@ package com.liberymutual.goforcode.romanNumerals.services;
 
 public class HinduToRomanNumeralConverter {
 	
+	String result;
+	
 	public HinduToRomanNumeralConverter() {
-		String[] symbols = {"I", "V", "X", "L", "C", "D", "M"}; //length of 7
-		String[] getNumber = {"1", "2", "3", "4", "5", "6", "7", "8", "9"};
-		String result = "";
-
-//		I("I", 1),
-//		V("V", 5),
-//		X("X", 10),
-//		L("L", 50),
-//		C("C", 100),
-//		D("D", 500),    
-//		M("M", 1000);
+		result = "";
 	}
 
 	public String convert(int numberToConvert) {
-		//1 = I
-		//4 = I + V
-		//for loop
+		
+		while(numberToConvert >= 1000) {
+			result += "M";
+			numberToConvert -= 1000;
 		}
-		return null;
+		while(numberToConvert >= 900) {
+			result += "CM";
+			numberToConvert -= 900;
+		}
+		while(numberToConvert >= 500) {
+			result += "D";
+			numberToConvert -= 500;
+		}
+		while(numberToConvert >= 400) {
+			result += "CD";
+			numberToConvert -= 400;
+		}
+		while(numberToConvert >= 100) {
+			result += "C";
+			numberToConvert -= 100;
+		}
+		while(numberToConvert >= 90) {
+			result += "XC";
+			numberToConvert -= 90;
+		}
+		while(numberToConvert >= 50) {
+			result += "L";
+			numberToConvert -= 50;
+		}
+		while(numberToConvert >= 40) {
+			result += "XL";
+			numberToConvert -= 40;
+		}
+		while(numberToConvert >= 10) {
+			result += "X";
+			numberToConvert -= 10;
+		}
+		while(numberToConvert >= 9) {
+			result += "IX";
+			numberToConvert -= 9;
+		}
+		while(numberToConvert >= 5) {
+			result += "V";
+			numberToConvert -= 5;
+		}
+		while(numberToConvert >=4) {
+			result += "IV";
+			numberToConvert -= 4;
+		}
+		while(numberToConvert >= 1) {
+			result += "I";
+			numberToConvert -= 1;
+		}
+		
+		return result;
+		}
 	}
 
-}
+
